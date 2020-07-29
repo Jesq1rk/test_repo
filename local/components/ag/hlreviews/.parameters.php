f22cf57e04647b4a41402976f55abaa5de8827a3
Jesq1rk <46454430+Jesq1rk@users.noreply.github.com>
2020-07-29 14:13:43 +0300

    Create .parameters.php

---
+++ local/components/ag/hlreviews/.parameters.php
@@ -0,0 +1,35 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+$arComponentParameters = array(
+  "GROUPS" => array(),
+  "PARAMETERS" => array(
+    "HL_ID" => array(
+      "PARENT" => "BASE",
+      "NAME" => "HL_ID",
+      "TYPE" => "STRING",
+      "MULTIPLE" => "N",
+      "DEFAULT" => "1",
+    ),
+    "ID" => array(
+      "PARENT" => "BASE",
+      "NAME" => "ID",
+      "TYPE" => "STRING",
+      "MULTIPLE" => "N",
+      "DEFAULT" => "1",
+    ),
+    "ENT_TYPE" => array(
+      "PARENT" => "BASE",
+      "NAME" => "ENT_TYPE",
+      "TYPE" => "STRING",
+      "MULTIPLE" => "N",
+      "DEFAULT" => "1",
+    ),
+    "URL_TO_REVIEW" => array(
+      "PARENT" => "BASE",
+      "NAME" => "URL_TO_REVIEW",
+      "TYPE" => "STRING",
+      "MULTIPLE" => "N",
+      "DEFAULT" => "1",
+    )
+  ),
+);
+?>
