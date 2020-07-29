4fff1e47bb8059fb873606fbc3113570b132befc
Jesq1rk <46454430+Jesq1rk@users.noreply.github.com>
2020-07-29 14:13:16 +0300

    Create .description.php

---
+++ local/components/ag/hlreviews/.description.php
@@ -0,0 +1,13 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+$arComponentDescription = array(
+"NAME" => GetMessage("Отзывы"),
+"DESCRIPTION" => GetMessage("Выдаем отзывы"),
+"PATH" => array(
+"ID" => "ag",
+"CHILD" => array(
+"ID" => "hlreviews",
+"NAME" => "Отзывы"
+)
+),
+"ICON" => "/images/icon.gif",
+);
